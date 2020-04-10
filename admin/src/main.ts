3aa3c5fc9b44dc97674dba19d1e666bb0f09a440
hmbool <1298541373@qq.com>
2020-04-10 17:24:46 +0800

    后台

---
+++ admin/src/main.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import axios from 'axios'
+import App from './App.vue'
+import './plugins/element'
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+Vue.prototype.$http = axios.create(
+  {
+    // baseURL: 'https://some-domain.com/api/',
+    baseURL: 'http://localhost:3000'
+  }
+)
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ admin/src/views/Main.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <!-- Aside content -->
+      <!-- vertical 垂直   指定到当前页面  router 表示是个路由菜单-->
+      <el-menu mode="vertical" style="height:100vh;" :default-active="$router.path" router >
+        <el-submenu
+          v-for="(item, index) in menu.items"
+          :index="`menu-item-${index}`"
+          :key="`menu-item-${index}`"
+        >
+          <template slot="title">{{item.title}}</template>
+          <!-- index 路由 -->
+          <el-menu-item
+            v-for="(subItem, subIndex) in item.items"
+            :key="`menu-item-${index}-${subIndex}`"
+            :index="subItem.path"  
+          >{{subItem.title}}</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <!-- Header content -->
+        视频 - 后台管理界面
+      </el-header>
+      <el-main>
+        <!-- Main content -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+// 标记组件
+@Component({})
+export default class Main extends Vue {
+  menu = {
+    items: [
+      {
+        title: "内容管理",
+        items: [
+          { title: "首页", path: "/" },
+          { title: "课程管理", path: "/courses/list" },
+          { title: "课时管理", path: "/courses/list" },
+        ]  // 子菜单
+      },
+      {
+        title: "运营管理",
+        items: [
+          { title: "用户管理", path: "/users/list" },
+        ]  // 子菜单
+      },
+    ]
+  };
+}
+</script>
+
+<style>
+</style>
